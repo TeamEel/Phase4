8d3a5b7af79aefe261f53be0780b0654f3fee8cc
Jonny Gilchrist <jonnygilchrist@gmail.com>
2013-02-03 14:27:15 +0000

    Moved game into textgame to retain text interface and created new Game.java for GUI

--- src/main/java/cichlid/seprphase3/GUIInterface/GUIInterface.java
+++ src/main/java/cichlid/seprphase3/GUIInterface/GUIInterface.java
@@ -1,6 +1,20 @@
 package cichlid.seprphase3.GUIInterface;
 
+import cichlid.seprphase3.Simulator.GameManager;
+import cichlid.seprphase3.Simulator.PlantController;
+import cichlid.seprphase3.Simulator.PlantStatus;
+import cichlid.seprphase3.Simulator.SoftwareFailure;
+import cichlid.seprphase3.Utilities.Pressure;
 
 public class GUIInterface {
 
+    private PlantController plantController;
+    private PlantStatus plantStatus;
+    private GameManager gameManager;
+
+    public GUIInterface(PlantController plantController, PlantStatus plantStatus, GameManager gameManager) {
+        this.plantController = plantController;
+        this.plantStatus = plantStatus;
+        this.gameManager = gameManager;
+    }
 }
