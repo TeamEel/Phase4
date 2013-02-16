eae6a6402ffc8cda993dec97d87fb7c00202f259
Jonny Gilchrist <jonnygilchrist@gmail.com>
2013-02-16 14:38:37 +0000

    Added some code to control animations and moved image functions into ImageUtils

--- src/main/java/cichlid/seprphase3/GUIInterface/Animation.java
+++ src/main/java/cichlid/seprphase3/GUIInterface/Animation.java
@@ -1,5 +1,47 @@
 package cichlid.seprphase3.GUIInterface;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Animation {
+    private BufferedImage[] images;
+    private int currentFrame;
     
+    static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+        @Override
+        public boolean accept(final File dir, final String name) {
+            String extension = ".png";
+            if(name.endsWith(extension)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+    
+    public Animation(String filePath) {
+        File imgDir = new File("images/" + filePath);
+        
+        try {
+            images = new BufferedImage[imgDir.listFiles().length];
+            int i = 0;
+            for(File f : imgDir.listFiles(IMAGE_FILTER)) {
+                images[i] = ImageUtils.loadImage(f.getCanonicalPath());
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to load image: " + e.getMessage());
+        }
+    }
+    
+    public BufferedImage stepImage() {
+        currentFrame++;
+        return images[currentFrame];
+    }
+    
+    public BufferedImage staticImage() {
+        return images[0];
+    }
 }
--- src/main/java/cichlid/seprphase3/GUIInterface/PlantGUIElement.java
+++ src/main/java/cichlid/seprphase3/GUIInterface/PlantGUIElement.java
@@ -10,6 +10,7 @@ public class PlantGUIElement {
     // This rectangle will  be used to detect mouse collision with PlantGUIComponents.
     public Rectangle location;
     // This is the image that will be drawn to the screen while the PlantGUIComponent is static.
+    public BufferedImage image;
     public Animation working;
     public Animation starting;
     public Animation stopping;
@@ -17,26 +18,13 @@ public class PlantGUIElement {
     public Animation meltdown;
     
     public PlantGUIElement(BufferedImage _image, int x, int y, float scaling) {
-        image = scaleImage(_image, scaling);
+        image = ImageUtils.scaleImage(_image, scaling);
         location = new Rectangle(x, y, image.getWidth(), image.getHeight());
     }
     
     public PlantGUIElement(BufferedImage _image, int x, int y, float scaling, int offsetx, int offsety) {
-        image = scaleImage(_image, scaling);
+        image = ImageUtils.scaleImage(_image, scaling);
         location = new Rectangle(x+offsetx, y+offsety, image.getWidth(), image.getHeight());
-    }
-    
-    /**
-     * Scales the image by a specified amount.
-     * @param unscaledImage     The image to scale
-     * @param percent           The percentage by which to scale it (1.0 is 100%).
-     * @return  A bufferedimage representing the scaled image.
-     */
-    private BufferedImage scaleImage(BufferedImage unscaledImage, float percent) {
-        Image scaledImage = unscaledImage.getScaledInstance( (int)(unscaledImage.getWidth() * percent), -1, Image.SCALE_SMOOTH);
-        BufferedImage scaledBufferedImage = new BufferedImage(scaledImage.getWidth(null), scaledImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-        scaledBufferedImage.getGraphics().drawImage(scaledImage, 0, 0, null);
-        return scaledBufferedImage;
     }
     
     public int x() {
